8aa35c737d470fa1820bda7d5e0dc3ccfba5b0e2
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-03 12:31:33 +0530

    add headers

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://chat-app-frontend-eosin.vercel.app"
+	);
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	next();
+});
+
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 
