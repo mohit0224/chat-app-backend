9c5a5be67688b9943d80365cd70ab2d7f690c0cd
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-04 13:13:43 +0530

    update isRedirected

--- middlewares/isRedirect.middleware.js
+++ middlewares/isRedirect.middleware.js
@@ -1,19 +1,14 @@
-import logger from "../logger.js";
-
 const isProduction = process.env.NODE_ENV === "production";
-logger.info(process.env.NODE_ENV);
 
 const isRedirect = (req, res, next) => {
 	if (isProduction) {
 		if (req.secure) {
-			logger.info(req.secure);
-
 			return next();
 		}
 		res.redirect(`https://${req.headers.host}${req.url}`);
+	} else {
+		next();
 	}
-
-	next();
 };
 
 export default isRedirect;
