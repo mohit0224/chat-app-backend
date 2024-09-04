d6d1077045bdc757c445c038f23c485759abd9ec
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-04 13:19:27 +0530

    update - isRedirect

--- middlewares/isRedirect.middleware.js
+++ middlewares/isRedirect.middleware.js
@@ -1,14 +1,8 @@
-const isProduction = process.env.NODE_ENV === "production";
-
 const isRedirect = (req, res, next) => {
-	if (isProduction) {
-		if (req.secure) {
-			return next();
-		}
-		res.redirect(`https://${req.headers.host}${req.url}`);
-	} else {
-		next();
+	if (req.secure) {
+		return next();
 	}
+	res.redirect(`https://${req.headers.host}${req.url}`);
 };
 
 export default isRedirect;
