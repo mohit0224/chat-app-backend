9269c6ffd2ffa62c71ab7b11df89213bbd935ebb
mohit0224 <mohitdheer24+git@gmail.com>
2024-09-05 10:05:23 +0530

    update cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -2,14 +2,17 @@ const allowedOrigins = process.env.CORS_ORIGIN;
 
 const corsOption = {
 	origin: function (origin, callback) {
-		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) !== -1) {
+		if (!origin || allowedOrigins.includes(origin)) {
 			callback(null, true);
 		} else {
 			callback(new Error("Not allowed by CORS"));
 		}
 	},
+	methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+	allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
 	credentials: true,
+	optionsSuccessStatus: 204,
+	maxAge: 600,
 };
 
 export default corsOption;
